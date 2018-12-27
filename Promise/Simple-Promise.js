d7613088faee69da0e98da699daa780219d17bba
“Jeremy” <zhuguogen@qq.com>
2018-12-27 22:43:20 +0800

    add Promise.all && Promise.race implements

--- Promise/Simple-Promise.js
+++ Promise/Simple-Promise.js
@@ -41,6 +41,7 @@ MyPromise.prototype.then = function (fn, onResolved, onRejected) {
   onResolved = typeof onResolved === 'function' ? onResolved : v => v
   onRejected = typeof onRejected === 'function' ? onRejected : r => { throw r }
 
+  
   if (_this.state === PENDING) {
     _this.resolvedCallbacks.push(onResolved)
     _this.rejectedCallbacks.push(onRejected)
@@ -53,4 +54,37 @@ MyPromise.prototype.then = function (fn, onResolved, onRejected) {
   if (_this.state === REJECTED) {
     onRejected(_this.value)
   }
+}
+
+// 返回一个没有 resolve 的 then 结果
+MyPromise.prototype.catch = function (callback) {
+  return this.then(null, callback)
+}
+
+// 接受一个 promises 数组, 全部完成再 resolve
+MyPromise.all = function (promises) {
+  return new MyPromise(function (resolve, reject) {
+    let arr = [] // 最终返回值的结果
+    let i = 0    // 成功了多少次
+    function processData (index, y) {
+      arr[index] = y
+      if (++i === promises.length) {
+        resolve(arr)
+      }
+    }
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(function (y) {
+        processData(i, y)
+      }, reject)
+    }
+  })
+}
+
+// 接受一个 promises 数组, 有一个成功就成功, 如果第一个失败就失败
+MyPromise.race = function (promises) {
+  return new MyPromise(function (resolve, reject) {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(resolve, reject)
+    }
+  })
 }
