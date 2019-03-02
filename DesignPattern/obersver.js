8c127fc962be5c2dbf8bc36820790684916e9695
“Jeremy” <zhuguogen@qq.com>
2019-03-02 20:40:02 +0800

    add defineProperty && proxy for obersver pattern

--- DesignPattern/obersver.js
+++ DesignPattern/obersver.js
@@ -4,7 +4,40 @@
  */
 
 // 点击一个按钮触发了事件
-let ul = document.createElement('ul')
-ul.addEventListener('click', event => {
-  console.log(event.target)
+// let ul = document.createElement('ul')
+// ul.addEventListener('click', event => {
+//   console.log(event.target)
+// })
+
+// Vue 双向绑定的核心
+// var obj = {
+//   list: []
+// }
+
+// var val = obj['list']
+// Object.defineProperty(obj, 'list', {
+//   get () {
+//     return val
+//   },
+//   set (newVal) {
+//     console.log('值被更改为: ', newVal)
+//     if (newVal !== val) val = newVal
+//   }
+// })
+
+// obj.list = [1, 2, 3]
+// console.log(obj.list)
+
+// Proxy
+var obj = {
+  value: 0
+}
+
+var proxy = new Proxy(obj, {
+  set: function (target, key, value, receiver) {
+    console.log('Set')
+    Reflect.set(target, key, value, receiver)
+  }
 })
+
+proxy.value = 1
